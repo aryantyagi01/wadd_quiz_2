eb5c3ec2eb35c302bad71b0aef2c43306f3d946f
aryantyagi01 <95321580+aryantyagi01@users.noreply.github.com>
2022-01-31 22:46:45 -0800

    Add files via upload

---
+++ array_question.js
@@ -0,0 +1,12 @@
+
+const array = ['John', 'John', 'Megan', 'Megan', 'Megan', 'Yasmine', 'Yasmine', 'Nelly'];
+
+console.log(array.indexOf('John'));
+
+console.log(array.indexOf('Megan'));
+
+console.log(array.indexOf('Yasmine'));
+
+console.log(array.indexOf('Nelly'));
+
+    
---
+++ string_question.js
@@ -0,0 +1,15 @@
+function humanize_format (number) {
+    if(number % 100 >= 11 && number % 100 <= 13)
+    return number + "th";
+
+     switch(number % 10) {
+         case 1: return number + "st";
+         case 2: return number + "nd";
+         case 3: return number + "rd";
+
+     }
+     return number + "th";
+     
+}
+console.log (humanize_format(1));
+console.log (humanize_format(8)); 
